1296d0e768f4f69b4a96ecd648b54c9ac2181051
Michael Graves <mdgrav@protonmail.com>
2021-10-15 18:01:05 -0500

    Turned off echo in terminal

--- tree.c
+++ tree.c
@@ -1,6 +1,22 @@
+#include <termios.h>
 #include <unistd.h>
 
+void enableRawMode() {
+    struct termios raw;
+
+    // Read terminal attributes
+    tcgetattr(STDIN_FILENO, &raw);
+
+    // Disables each key typed to be printed to the terminal
+    raw.c_lflag &= ~(ECHO);
+
+    // Apply terminal attributes
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
 int main() {
+    enableRawMode();
+
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
