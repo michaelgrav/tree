d9d7c520224de4e718aa67b9b5adc15da67305ca
Michael Graves <mdgrav@protonmail.com>
2021-10-15 21:55:07 -0500

    Added error handling

--- tree.c
+++ tree.c
@@ -1,4 +1,5 @@
 #include <ctype.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <termios.h>
@@ -6,13 +7,20 @@
 
 struct termios orig_termios;
 
+// Error handling
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawMode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == 1)
+        atexit(disableRawMode);
 }
 
 void enableRawMode() {
     // Read terminal attributes
-    tcgetattr(STDIN_FILENO, &orig_termios);
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
     struct termios raw = orig_termios;
@@ -25,15 +33,15 @@ void enableRawMode() {
     raw.c_cc[VTIME] = 1;
 
     // Apply terminal attributes
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
 int main() {
     enableRawMode();
-    
+
     while (1) {
         char c = '\0';
-        read(STDIN_FILENO, &c, 1);
+        if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } 
