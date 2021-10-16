2627898ce1576cd7afe8298a3483884d2e342a24
Michael Graves <mdgrav@protonmail.com>
2021-10-15 19:51:47 -0500

    Added timeout for read()

--- tree.c
+++ tree.c
@@ -17,9 +17,12 @@ void enableRawMode() {
 
     struct termios raw = orig_termios;
     // Disables flags and fixes control keys
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     // Apply terminal attributes
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -29,13 +32,16 @@ int main() {
     enableRawMode();
 
     char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } 
         else {
             printf("%d ('%c')\r\n", c, c);
         }
+        if (c != 'q') break;
     }
     return 0;
 }
