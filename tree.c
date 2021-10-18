eb4c544c082295a04d058e55cc85a17ff1a90417
Michael Graves <mdgrav@protonmail.com>
2021-10-18 12:52:27 -0500

    Limited cursor to only go to end of line

--- tree.c
+++ tree.c
@@ -294,7 +294,8 @@ void editorRefreshScreen() {
     editorDrawRows(&ab);
 
     char buf[32];
-    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff)+ 1, E.cx + 1);
+    snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff)+ 1, 
+                                              (E.cx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -306,6 +307,8 @@ void editorRefreshScreen() {
 /*** input ***/
 
 void editorMoveCursor(int key) {
+    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+
     switch (key) {
     case ARROW_LEFT:
         if (E.cx != 0) {
@@ -313,7 +316,9 @@ void editorMoveCursor(int key) {
         }
         break;
     case ARROW_RIGHT:
+        if (row && E.cx < row->size) {
             E.cx++;
+        }
         break;
     case ARROW_UP:
         if (E.cy != 0) {
