8cc97a8ad9331eedf6bb5f8a515381efc518c7c7
Michael Graves <mdgrav@protonmail.com>
2021-10-18 13:21:08 -0500

    Cursor now moves properly when tabs are present

--- tree.c
+++ tree.c
@@ -46,6 +46,7 @@ typedef struct erow {
 
 struct editorConfig {
     int cx, cy;
+    int rx;
     int rowoff;
     int coloff;
     int screenrows;
@@ -178,6 +179,17 @@ int getWindowSize(int *rows, int *cols) {
 
 /*** row operations ***/
 
+int editorRowCxToRx(erow *row, int cx) {
+    int rx = 0;
+    int j;
+    for (j = 0; j < cx; j++) {
+        if (row->chars[j] == '\t')
+            rx+= (TREE_TAB_STOP - 1) - (rx % TREE_TAB_STOP);
+        rx++;
+    }
+    return rx;
+}
+
 void editorUpdateRow(erow *row) {
     int tabs = 0;
     int j;
@@ -261,6 +273,11 @@ void abFree(struct abuf *ab) {
 /*** output ***/
 
 void editorScroll() {
+    E.rx = 0;
+    if (E.cy < E.numrows) {
+        E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+    }
+
     //Vertical
     // If the cursor goes above the top
     if (E.cy < E.rowoff) {
@@ -272,11 +289,11 @@ void editorScroll() {
     }
     
     //Horizontal
-    if (E.cx < E.coloff) {
+    if (E.rx < E.coloff) {
         E.coloff = E.cx;
     }
     
-    if (E.cx >= E.coloff + E.screencols) {
+    if (E.rx >= E.coloff + E.screencols) {
         E.coloff = E.cx - E.screencols + 1;
     }
 }
@@ -327,7 +344,7 @@ void editorRefreshScreen() {
 
     char buf[32];
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff)+ 1, 
-                                              (E.cx - E.coloff) + 1);
+                                              (E.rx - E.coloff) + 1);
     abAppend(&ab, buf, strlen(buf));
 
     abAppend(&ab, "\x1b[?25h", 6);
@@ -419,6 +436,7 @@ void editorProcessKeypress() {
 void initEditor() {
     E.cx = 0;
     E.cy = 0;
+    E.rx = 0;
     E.rowoff = 0;
     E.coloff = 0;
     E.numrows = 0;
