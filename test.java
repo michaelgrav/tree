bef5227b2d10bdfb30a87257cfd63028e90caba7
Michael Graves <mdgrav@protonmail.com>
2021-10-18 19:50:16 -0500

    Tree now has the ability to save changes

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Denver");
+        list.add("Austin");
+        //list.add(new java.util.Date());
+        String city = list.get(0);
+        //list.set(3, "Dallas");
+        System.out.println(list.get(3));
+    }
+}
+// This is a test
+// Hello
--- tree.c
+++ tree.c
@@ -6,6 +6,7 @@
 
 #include <ctype.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
@@ -24,6 +25,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
+    BACKSPACE = 127,
     ARROW_LEFT = 1000,
     ARROW_RIGHT,
     ARROW_UP,
@@ -255,6 +257,25 @@ void editorInsertChar(int c) {
 
 /*** file i/o ***/
 
+char *editorRowsToString(int *buflen) {
+    int totlen = 0;
+    int j;
+    for (j = 0; j < E.numrows; j++)
+        totlen += E.row[j].size + 1;
+    *buflen = totlen;
+
+    char *buf = malloc(totlen);
+    char *p = buf;
+    for (j = 0; j < E.numrows; j++) {
+        memcpy(p, E.row[j].chars, E.row[j].size);
+        p += E.row[j].size;
+        *p = '\n';
+        p++;
+    }
+
+    return buf;
+}
+
 void editorOpen(char *filename) {
     free(E.filename);
     E.filename = strdup(filename);
@@ -273,6 +294,19 @@ void editorOpen(char *filename) {
     }
     free(line);
     fclose(fp);
+}
+
+void editorSave() {
+    if (E.filename == NULL) return;
+
+    int len;
+    char *buf = editorRowsToString(&len);
+
+    int fd = open(E.filename, O_RDWR | O_CREAT, 0644);
+    ftruncate(fd, len);
+    write(fd, buf, len);
+    close(fd);
+    free(buf);
 }
 
 /*** append buffer ***/
@@ -467,10 +501,18 @@ void editorProcessKeypress() {
     int c = editorReadKey();
 
     switch (c) {
+    case '\r':
+        /* TODO */
+        break;
+
     case CTRL_KEY('q'):
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDERR_FILENO, "\x1b[H", 3);
         exit(0);
+        break;
+
+    case CTRL_KEY('s'):
+        editorSave();
         break;
 
     case HOME_KEY:
@@ -481,7 +523,13 @@ void editorProcessKeypress() {
             E.cx = E.row[E.cy].size;
         break;
 
-    
+    case BACKSPACE:
+    case CTRL_KEY('h'):
+    case DEL_KEY:
+        /* TODO */
+        break;
+
+
     case PAGE_UP:
     case PAGE_DOWN:
         {
@@ -506,6 +554,10 @@ void editorProcessKeypress() {
         editorMoveCursor(c);
         break;
     
+    case CTRL_KEY('l'):
+    case '\x1b':
+        break;
+
     default:
         editorInsertChar(c);
         break;
