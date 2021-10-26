6af10b533d624c398fec10a4b2aec1160b7647d2
Michael Graves <mdgrav@protonmail.com>
2021-10-26 10:16:53 -0500

    Added warining when quitting without saving

---
+++ test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Welcome to the C Language!\n");
+	return(0);
+}
+//Hello
--- tree.c
+++ tree.c
@@ -21,6 +21,7 @@
 
 #define VERSION_NO "0.0.1"
 #define TREE_TAB_STOP 8
+#define TREE_QUIT_TIMES 3
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -57,6 +58,7 @@ struct editorConfig {
     int screencols;
     int numrows;
     erow *row;
+    int dirty; //Used to define if there are changes not saved yet
     char *filename;
     char statusmsg[80];
     time_t statusmsg_time;
@@ -239,6 +241,7 @@ void editorAppendRow(char *s, size_t len) {
     editorUpdateRow(&E.row[at]);
 
     E.numrows++;
+    E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c) {
@@ -248,6 +251,7 @@ void editorRowInsertChar(erow *row, int at, int c) {
     row->size++;
     row->chars[at] = c;
     editorUpdateRow(row);
+    E.dirty++;
 }
 
 /*** editor operations ***/
@@ -299,6 +303,7 @@ void editorOpen(char *filename) {
     }
     free(line);
     fclose(fp);
+    E.dirty = 0;
 }
 
 void editorSave() {
@@ -316,6 +321,7 @@ void editorSave() {
             {
                 close(fd);
                 free(buf);
+                E.dirty = 0;
                 editorSetStatusMessage("%d bytes written to disk", len);
                 return;
             }
@@ -413,7 +419,8 @@ void editorDrawStatusBar(struct abuf * ab) {
     char status[80], rstatus[80];
 
     int len = snprintf(status, sizeof(status), "%.20s - %d lines",
-        E.filename ? E.filename : "[No Name]", E.numrows);
+        E.filename ? E.filename : "[No Name]", E.numrows,
+        E.dirty ? "(modified)" : "");
 
     int rlen = snprintf(rstatus, sizeof(rstatus), "%d/%d",
         E.cy + 1, E.numrows);
@@ -515,6 +522,8 @@ void editorMoveCursor(int key) {
 
 // Handles keypresses
 void editorProcessKeypress() {
+    static int quit_times = TREE_QUIT_TIMES;
+
     int c = editorReadKey();
 
     switch (c) {
@@ -523,6 +532,13 @@ void editorProcessKeypress() {
         break;
 
     case CTRL_KEY('q'):
+        if (E.dirty && quit_times > 0)
+        {
+            editorSetStatusMessage("WARNING! File has unsaved changes. "
+            "Press Ctrl-Q %d more times to quit.", quit_times);
+            quit_times--;
+            return;
+        } 
         write(STDOUT_FILENO, "\x1b[2J", 4);
         write(STDERR_FILENO, "\x1b[H", 3);
         exit(0);
@@ -579,6 +595,8 @@ void editorProcessKeypress() {
         editorInsertChar(c);
         break;
     }
+
+    quit_times = TREE_QUIT_TIMES;
 }
 
 /*** init ***/
@@ -591,6 +609,7 @@ void initEditor() {
     E.coloff = 0;
     E.numrows = 0;
     E.row = NULL;
+    E.dirty = 0;
     E.filename = NULL;
     E.statusmsg[0] = '\0';
     E.statusmsg_time = 0;
@@ -606,7 +625,7 @@ int main(int argc, char *argv[]) {
         editorOpen(argv[1]);
     }
 
-    editorSetStatusMessage("HELP: Ctrl-s = save | Ctrl-Q = quit");
+    editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit");
 
     while (1) {
         editorRefreshScreen();
